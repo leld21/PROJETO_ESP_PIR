d42c801fbbe5b71ecfa64d70cd1cab7bafaf277d
leld21 <leandro7531591@gmail.com>
2021-06-29 01:35:16 -0300

    Update alerta_de_atividade.cpp

--- alerta_de_atividade.cpp
+++ alerta_de_atividade.cpp
@@ -3,3 +3,27 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
+const char* ID = "PORTALNET_D088";
+const char* SENHA = "18008920";
+
+const int sensor=11;
+bool mov_detectado = false;
+
+void ICACHE_RAM_ATTR DetectaMovimento() {
+
+  mov_detectado = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(sensor, INPUT_PULLUP);
+
+
+}
+
+void loop(){
+    
+}
+
+
+  
