22345b02ea931c01e88148f3203385c0af97ef06
leld21 <leandro7531591@gmail.com>
2021-07-01 22:53:19 -0300

    Update alerta_de_atividade.cpp

--- alerta_de_atividade.cpp
+++ alerta_de_atividade.cpp
@@ -3,13 +3,23 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
+/* Leandro Santana
+    Projeto final da matéria Fundamentos de Sistemas Embarcados
+    Utilizando ESP8266, sensor PIR e um BOT do Telegram.
+*/
+
+
+//ID e senha do wifi
 const char* ID = "PORTALNET_D088";
 const char* SENHA = "18008920";
 
-const int sensor=11;
+//Definindo o sensor no pino D5
+const int sensor=D5;
+//iniciando a variavel de movimento detectado como falso
 bool mov_detectado = false;
 
-void ICACHE_RAM_ATTR DetectaMovimento() {
+//Função para modificar a variavel de movimento para verdade ( utiliza o cache da RAM interna )
+void IRAM_ATTR DetectaMovimento() {
   mov_detectado = true;
 }
 
